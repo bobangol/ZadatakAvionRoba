0806730c7f5dbfc82c25d73e2c6ef50ca642e017
bobangol <gboban@gmail.com>
2018-05-14 18:53:56 +0200

    Uradjen zadatk 2 closes #2

---
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -0,0 +1,18 @@
+package zadaci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+
+import java.sql.SQLException;
+
+public class Zadatak4BrisanjeVrednosti {
+    public static void main(String[] args) {
+
+        JdbcConnectionSource connectionSource = null;
+
+        try {
+            connectionSource = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
