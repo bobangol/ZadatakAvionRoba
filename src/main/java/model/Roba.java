27880c23dbc1139722a139086c7eda5f220ca87c
bobangol <gboban@gmail.com>
2018-05-14 19:29:32 +0200

    Uradjen zadatak 2.1.1. closes #3

---
+++ src/main/java/model/Roba.java
@@ -0,0 +1,60 @@
+package model;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.stmt.query.ColumnNameOrRawSql;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "roba")
+public class Roba {
+
+    public static final String POLJE_NAZIV="naziv";
+    public static final String POLJE_OPIS="opis";
+    public static final String POLJE_TEZINA = "tezina";
+
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField(columnName = POLJE_NAZIV,canBeNull = false, unique=false)
+    private String naziv;
+    @DatabaseField(columnName = POLJE_OPIS,canBeNull = false, unique=false)
+    private String opis;
+    @DatabaseField(columnName = POLJE_TEZINA,canBeNull = false)
+    private  double tezina;
+
+
+    public Roba(){
+
+    }
+
+    public Roba(String naziv, String opis, double tezina){
+        this.naziv = naziv;
+        this.opis = opis;
+        this.tezina = tezina;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public double getTezina() {
+        return tezina;
+    }
+
+    public void setTezina(double tezina) {
+        this.tezina = tezina;
+    }
+
+
+}
